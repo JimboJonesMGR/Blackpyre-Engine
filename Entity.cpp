cfbe2994b6eb1d5055e0def20945338f022094ad
HFLW <me@hflw.org>
2010-08-31 01:59:35 -0400

    worked on entity management and some other stuff, no longer compiles. Need to figure out some compiling errors

---
+++ Entity.cpp
@@ -0,0 +1,9 @@
+/*
+ * Blackpyre Engine
+ * Copyright (c) 2010 HFLW. All rights reserved.
+ * 
+ * Source available under the Academic Free License
+ * See LICENSE for licensing information
+ */
+
+#include "Entity.h" 
